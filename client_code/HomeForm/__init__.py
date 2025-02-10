93363850de49bd05f40fce1db74fe26b45185750
grippe@blue-way.net <grippe@blue-way.net>
2025-02-10 15:30:05 +0000

    Edited theme.css and HomeForm

--- client_code/HomeForm/__init__.py
+++ client_code/HomeForm/__init__.py
@@ -3,6 +3,7 @@ from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import webbrowser
 
 
 class HomeForm(HomeFormTemplate):
@@ -13,4 +14,11 @@ class HomeForm(HomeFormTemplate):
     # Any code you write here will run before the form opens.
 
   def btn_thanks_click(self, **event_args):
-    alert(content="To our Alpha testers, Baesweiler", title="Thank you", large=True)
+    alert(content="... to our Alpha testers, the students in course SW101 at the Realschule Baesweiler during April 2024 taught by René Langohr, and all the beta testers.", title="Thank you", large=True)
+
+  def btn_help_click(self, **event_args):    
+    webbrowser.open_new("http://sdggamehelp.blue-way.net")
+
+  def btn_admin_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass

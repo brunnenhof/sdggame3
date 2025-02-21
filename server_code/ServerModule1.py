505e15d9f37dec6ed4ccae5f3105d73aa801d1e3
grippe@blue-way.net <grippe@blue-way.net>
2025-02-21 18:32:51 +0000

    Edited ServerModule1 and HomeForm.one_plot

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -232,7 +232,7 @@ def get_play_25():
 
 def get_reg_x_name_colx(region):
   row = app_tables.regions.get(abbreviation=region)
-  regix = row['id']
+  regix = int(row['id'])
   long = row['name']
   farbe = row['color']
   return regix, long, farbe
@@ -273,46 +273,123 @@ def read_mdf25():
     mdf = np.array(mdf_read)
   return mdf
 
-def make_png(df, row):
+def pick(ys, x, y):
+  o = []
+  for i in range(0, len(ys)):
+    idx = np.where(x==i)[0][0]
+    lo = y.iloc[idx]
+    o.append(lo)
+  return o
+  
+def make_png(df, row, pyidx, end_yr):
   fig = plt.figure()
   x = df[:, 0]
   y = df[:, 1]  
-  plt.plot(x, y)  
-  plt.xlabel('Years')  
-  plt.ylabel('')  
-  fig.tight_layout(pad=15)
+  data_max = y.max() * 1.1
+  data_min = y.min()
+  plot_max = row['ymax']
+  plot_min = row['ymin']
+  ymin = min(data_min, plot_min)
+  ymax = max(data_max, plot_max)
+  if ymin > 0:
+    ymin = 0
+  if ymax < 0:
+    ymax = 0
+  if int(row['id']) in [26, 4]:  # Labour share of GDP | life expectancy
+    ymin = plot_min # red min
+  if int(row['id']) in [25,31]: # population | Nitrogen use
+    ymax = data_max
+  dt = 1 / 32  # = 0.03125
+  abc = app_tables.regions.get(pyidx=pyidx)
+  my_colhex = abc['colhex']
+  my_lab = abc['name']
+  plt.plot(x, y, color=my_colhex, linewidth=2.5, label=my_lab)
+  # now plot the thick dots
+  # get the year picks
+  runto_row = app_tables.runto.get(end_year=end_yr)
+  yr_picks_str = runto_row['yr_picks']
+  yps = yr_picks_str.replace("'", "")
+  yr_picks = yps.split(' ')
+  yps_int = []
+  for i in range(0,len(yr_picks)):
+    yps_int.append(int(yr_picks[i]))
+  print ('IN make_png yr_picks: ')
+  pvt_len = len(yr_picks)
+  ys = pick(yps_int, x, y)
+  plt.scatter(yr_picks, ys, color = my_colhex, s=66, alpha=0.5)
+  if int(row['lowerbetter']) == 1:
+    grn_min = row['ymin'] # 8
+    grn_max = row['green'] # vars_df.iloc[varx, 4]
+    red_min = row['red'] #vars_df.iloc[varx, 5]
+    if int(row['id']) == 16: # Emissions per person
+      red_max = max(data_max, 8)
+      ymax = red_max
+    else:
+      red_max = row['ymax'] # vars_df.iloc[varx, 9]
+    if red_max < ymax:
+      red_max = ymax
+    yel_min = grn_max
+    yel_max = red_min
+  else:
+    red_min = row['ymin'] # vars_df.iloc[varx, 8]
+    if int(row['id']) == 9: # Access to electricity
+      if red_min > ymin:
+        ymin = red_min
+    red_max = row['red'] # vars_df.iloc[varx, 5]
+    grn_min = row['green'] #vars_df.iloc[varx, 4]
+    grn_max = row['ymax'] #vars_df.iloc[varx, 9]
+    yel_min = red_max
+    yel_max = grn_min
+
+    plt.ylim(ymin, ymax)
+    xmin = 1990
+    xmax = end_yr
+    if not int(row['id']) == 25: # population
+        opa = 0.075
+        plt.fill_betweenx(y=[grn_min, grn_max], x1=xmin, x2=xmax, color='green', alpha=opa)
+        plt.fill_betweenx(y=[red_min, red_max], x1=xmin, x2=xmax, color='red', alpha=opa)
+        plt.fill_betweenx(y=[yel_min, yel_max], x1=xmin, x2=xmax, color='yellow', alpha=opa)
+
+#    plt.figtext(0.88, 0.035, 'ug ' + cap, ha='right', fontsize=8)
+    plt.grid(color='gainsboro', linestyle='-', linewidth=.5)
+        # leg = plt.legend()
+        # leg.get_frame().set_alpha(.8)
+    plt.box(False)
+
+#  plt.plot(x, y)  
+#  plt.xlabel('Years')  
+#  plt.ylabel('')  
+#  fig.tight_layout(pad=15)
   return anvil.mpl_util.plot_image()
+
+#    plt.xticks(yr_points)
+#    plt.subplots_adjust(top=0.83, bottom=0.15)
+
   
 @anvil.server.background_task
 @anvil.server.callable
 def fake_it_server(region, single_ta):
   # region as 'nn' single ta as 'poverty', etc
+  print(region)
+  regrow = app_tables.regions.get(abbreviation=region)
+  print (regrow)
+  regidx = int(regrow['pyidx'])
   fcol_in_mdf = read_fcol_in_mdf()
-#  d_table = read_d_table()
-#  print (d_table)
   mdf = read_mdf25()
   num_rows, num_cols = mdf.shape
-# drop first 10 years from 1980 to 1990 to get the spin-up wrinkles out
   my_time = time.localtime()
   my_time_formatted = time.strftime("%a %d %b %G", my_time)
   foot1 = 'mov240906 mppy GAME e4a 10reg.mdl'
   cap = foot1 + ' on ' + my_time_formatted
   regidx, long, farbe = get_reg_x_name_colx(region)
-  print(region + '  ' + long)
-  print('    ' + single_ta)
-# get the names of all vars in the current TA / Ministry
+#  print(region + '  ' + long)
+#  print('    ' + single_ta)
   vars_info_l, vars_info_rows = get_all_vars_for_ta(single_ta)
   plot_list = []
   for var_row in vars_info_rows:
     var_l = var_row['vensim_name']
-#    print('IN fake_it_server, var_l: '+str(var_l))
     var_l = var_l.replace(" ", "_") # vensim uses underscores not whitespace in variable name
-    sdg_name = var_row['sdg_nbr']
-#    print('IN fake_it_server, sdg_name: '+str(sdg_name))
-    sdg_idx = var_row['id']
-#    print('IN fake_it_server, sdg_idx: '+str(sdg_idx))
     varx = var_row['id']
-#    print('IN fake_it_server, varx: '+str(varx))
     if varx in[18, 20, 34]: # global variable  
       idx = fcol_in_mdf[var_l] # find location of variable in mdf
     else:
@@ -320,170 +397,15 @@ def fake_it_server(region, single_ta):
 
 #    row = get_row_from_varl(var_l)
     print('IN fake_it_server, idx: ' + str(idx) + ' varl: ' + var_l)
-    dfv = mdf[:, [0, idx]]
-    cur_fig = make_png(dfv, var_row)
+    print('IN fake_it_server, idx: ' + str(idx) + ' regidx: ' + str(regidx))    
+    lx = idx + regidx
+    print (lx)
+    dfv = mdf[:, [0, lx]]
+    cur_fig = make_png(dfv, var_row, regidx, 2025)
     cur_title = 'ETI-' + str(int(var_row['sdg_nbr'])) + ': ' +var_row['sdg']
     cur_sub = var_row['indicator']
-    cur_unit = var_row['subtitle']
     cur_cap = cap
-    fdz = {'title' : cur_title, 'subtitle' : cur_sub, 'fig' : cur_fig, 'cap' : cur_cap, 'unit' : cur_unit}
+    fdz = {'title' : cur_title, 'subtitle' : cur_sub, 'fig' : cur_fig, 'cap' : cur_cap}
     plot_list.append(fdz)
   return plot_list
     
-#    print ('IN fake_it_server, dfv: ' )
-
-#    fig = generate_mpl()
-#    print(type(fig))
-#    full_dict = (
-#      {'title': 'Joe', 'subtitle': 'Latest tech report', 'fig' : fig, 'cap': 'caption'},
-#      {'title': 'Joe Blow', 'subtitle': 'Grandpa', 'fig' : fig,  'cap': 'caption 2'},
-#      {'title': 'Joe Blow jr', 'subtitle': 'Son', 'fig' : fig,  'cap': 'caption 23'},
-#      {'title': 'Joe Blow jr III', 'subtitle': 'Son of a b...', 'fig' : fig,  'cap': 'caption 234'},
-#    )
-#  
-#    sdg_idx = vars_info_l.iloc[i,0]
-#    varx_list = vars_df.index[vars_df['modelvariable'] == var_l].tolist()
-#    varx = varx_list[0] # make an integer
-#    print('        ', var_l, ' ', str(varx))
-#    if varx in[18, 20, 34]: # global variable
-#        var_l = var_l.replace(" ", "_")
-#        idx = fcol_in_mdf[var_l]
-#        dfv = mdf[:, idx]
-#        dfv = dfv[0:end_rowi-1]
-    # Define a dictionary containing Students data
-#        dfvpd = pd.DataFrame(dfv, columns=['glob'])
-#        dfvpd = dfvpd * vars_df.iloc[varx, 12]
-#        yr = np.arange(1990, end_yr, 0.03125)
-#        dfvpd.insert(loc=0, column='yr', value=yr)
-#        yr_py_int = np.int_(yr_py)
-#        pvt = np.full((lx, 1), np.nan)  # placeholder for year points
-#        for i in range(lx):
-#            idx = max(1, yr_py_int.item(i))
-#            pvt[i] = dfvpd.iloc[idx-1, 1]
-#            fn = folder + region + '-' + str(varx) + '-' + single_ta + '.png'
-#            plot_glob_ta_pol(dfvpd, pvt, varx, fn)
-#    else: # regional variable
-    # vensim uses underscores not whitespace in variable name
-#        var_l = var_l.replace(" ", "_")
-        # find location of variable in mdf
-#        idx = fcol_in_mdf[var_l]
-        # get the slice with all regional data for the variable
-#        dfv = mdf[:, idx:idx + 10]
-        # get the slice of rows
-#        dfv = dfv[0:end_rowi - 1, :]
-        # make a pd dataframe
-#        dfvpd = pd.DataFrame(dfv, columns=my_lab)
-        # scale
-#        dfvpd = dfvpd * vars_df.iloc[varx, 12]
-        # slice out the correct region column#
-#        dfvpd = pd.DataFrame(dfvpd.iloc[:, regidx])
-        # make a colum with correct time data
-#        yr = np.arange(1990, end_yr, 0.03125)
-        # put the time in slot 0
-#        dfvpd.insert(loc=0, column='yr', value=yr)
-        # fig = px.line(d3,x='yr',y='cn')
-        # fig.show()
-        # prepare the data for the years with thick dots
-#        yr_py_int = np.int_(yr_py)
-#        pvt = np.full((lx, 1), np.nan)  # placeholder for year points
-#        for i in range(lx):
-#            idx = max(1, yr_py_int.item(i))
-#            pvt[i] = dfvpd.iloc[idx-1, 1]
-        # prepare the correct filename
-#        lfn = region + '-' + str(varx) + '-' + single_ta + '.png'
-#        fn = os.path.join(cwd, folder, lfn)
-        # send for plotting
-#        plot_each_reg_ta_pol(dfvpd, pvt, varx, fn)
-#        plot_each_reg_ta_pol2(dfvpd, pvt, varx, fn)
-#    fig = generate_mpl()
-#    print(type(fig))
-#    full_dict = (
-#     {'title': 'Joe', 'subtitle': 'Latest tech report', 'fig' : fig, 'cap': 'caption'},
-#     {'title': 'Joe Blow', 'subtitle': 'Grandpa', 'fig' : fig,  'cap': 'caption 2'},
-#     {'title': 'Joe Blow jr', 'subtitle': 'Son', 'fig' : fig,  'cap': 'caption 23'},
-#     {'title': 'Joe Blow jr III', 'subtitle': 'Son of a b...', 'fig' : fig,  'cap': 'caption 234'},
-#   )
-#    return full_dict
-
-@anvil.server.callable
-def load_plots(region, single_ta):
-  # region as 'nn' single ta as 'poverty', etc
-  my_time = time.localtime()
-  my_time_formatted = time.strftime("%a %d %b %G", my_time)
-  foot1 = 'mov240906 mppy GAME e4a 10reg.mdl'
-  cap = foot1 + ' on ' + my_time_formatted
-#  mdf = get_play_25()
-#  num_rows, num_cols = mdf.shape
-# drop first 10 years from 1980 to 1990 to get the spin-up wrinkles out
-#  mdf = mdf[321:num_rows, :]
-  regidx, long, farbe = get_reg_x_name_colx(region)
-  print(region + '  ' + long)
-  print('    ' + single_ta)
-# get the names of all vars in the current TA / Ministry
-  vars_info_l = get_all_vars_for_ta(single_ta)
-  print('IN load_plots, vars_info_l')
-  print(vars_info_l)
-  title = 'test title ' + str(random.randint(100,999))
-  sub = 'test subtile ' + str(random.randint(100,999))
-  fig = 'test fig'
-  return title, sub, fig, cap
-#  for i in range(len(vars_info_l)):
-    # name of the vensim variable
-#    var_l = vars_info_l.iloc[i,3]
-#    sdg_name = vars_info_l.iloc[i,1]
-#    sdg_idx = vars_info_l.iloc[i,0]
-#    varx_list = vars_df.index[vars_df['modelvariable'] == var_l].tolist()
-#    varx = varx_list[0] # make an integer
-#    print('        ', var_l, ' ', str(varx))
-#    if varx in[18, 20, 34]: # global variable
-#        var_l = var_l.replace(" ", "_")
-#        idx = fcol_in_mdf[var_l]
-#        dfv = mdf[:, idx]
-#        dfv = dfv[0:end_rowi-1]
-    # Define a dictionary containing Students data
-#        dfvpd = pd.DataFrame(dfv, columns=['glob'])
-#        dfvpd = dfvpd * vars_df.iloc[varx, 12]
-#        yr = np.arange(1990, end_yr, 0.03125)
-#        dfvpd.insert(loc=0, column='yr', value=yr)
-#        yr_py_int = np.int_(yr_py)
-#        pvt = np.full((lx, 1), np.nan)  # placeholder for year points
-#        for i in range(lx):
-#            idx = max(1, yr_py_int.item(i))
-#            pvt[i] = dfvpd.iloc[idx-1, 1]
-#            fn = folder + region + '-' + str(varx) + '-' + single_ta + '.png'
-#            plot_glob_ta_pol(dfvpd, pvt, varx, fn)
-#    else: # regional variable
-    # vensim uses underscores not whitespace in variable name
-#        var_l = var_l.replace(" ", "_")
-        # find location of variable in mdf
-#        idx = fcol_in_mdf[var_l]
-        # get the slice with all regional data for the variable
-#        dfv = mdf[:, idx:idx + 10]
-        # get the slice of rows
-#        dfv = dfv[0:end_rowi - 1, :]
-        # make a pd dataframe
-#        dfvpd = pd.DataFrame(dfv, columns=my_lab)
-        # scale
-#        dfvpd = dfvpd * vars_df.iloc[varx, 12]
-        # slice out the correct region column#
-#        dfvpd = pd.DataFrame(dfvpd.iloc[:, regidx])
-        # make a colum with correct time data
-#        yr = np.arange(1990, end_yr, 0.03125)
-        # put the time in slot 0
-#        dfvpd.insert(loc=0, column='yr', value=yr)
-        # fig = px.line(d3,x='yr',y='cn')
-        # fig.show()
-        # prepare the data for the years with thick dots
-#        yr_py_int = np.int_(yr_py)
-#        pvt = np.full((lx, 1), np.nan)  # placeholder for year points
-#        for i in range(lx):
-#            idx = max(1, yr_py_int.item(i))
-#            pvt[i] = dfvpd.iloc[idx-1, 1]
-        # prepare the correct filename
-#        lfn = region + '-' + str(varx) + '-' + single_ta + '.png'
-#        fn = os.path.join(cwd, folder, lfn)
-        # send for plotting
-#        plot_each_reg_ta_pol(dfvpd, pvt, varx, fn)
-#        plot_each_reg_ta_pol2(dfvpd, pvt, varx, fn)
-
-  pass

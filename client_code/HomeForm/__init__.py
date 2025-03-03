a0d164e764ef9c07784b90908086434bc547f5f4
grippe@blue-way.net <grippe@blue-way.net>
2025-03-03 08:56:21 +0000

    Created HomeForm.pov_template; Edited HomeForm

--- client_code/HomeForm/__init__.py
+++ client_code/HomeForm/__init__.py
@@ -479,7 +479,7 @@ class HomeForm(HomeFormTemplate):
 #    app_tables.globs.delete_all_rows()
 #    app_tables.globs.add_row(game_id_pers=your_game_id,ta=which_ministry, 
 #        reg=which_region,runde=1, game_id=cid,updated=datetime.datetime.now())
-    self.fut_repeat_panel.items = fut_pol_list
+    self.pov_rep_panel.items = fut_pol_list
 #    anvil.server.call('put_budget', 2025, cid)
     
   def submit_role_click(self, **event_args):
@@ -539,6 +539,72 @@ class HomeForm(HomeFormTemplate):
     budget, pol_list = anvil.server.call('get_policy_budgets', '1', '2', 2025)
     print(pol_list)
     self.pol_repeat.items = pol_list
+
+  def put_policy_investments(self, **event_args):
+    global budget
+    row = app_tables.globs.get()
+    cid = row['game_id']
+    ta = row['ta'].capitalize()
+    reg = row['reg']
+    pol_list = []
+    pols = app_tables.policies.search(ta=ta)
+    for pol in pols:
+      print(pol)
+      pol_name = pol['name']
+#    print(pol_name)
+      pol_expl = pol['expl']
+      pol_tltl = pol['tltl']
+      pol_gl = pol['gl']
+      pol_abbr = pol['abbreviation']
+      fdz = {'pol_name' : pol_name, 'pol_expl' : pol_expl, 'pol_tltl' : pol_tltl, 'pol_gl' : pol_gl, 'pol_abbr' : pol_abbr}
+      pol_list.append(fdz)
+ 
+      pct_pov = [r['wert'] for r in app_tables.games.search(game_id=cid, ta='Poverty', reg=reg, runde=runde)]
+      pct_ineq = [r['wert'] for r in app_tables.games.search(game_id=cid, ta='Inequality', reg=reg, runde=runde)]
+      pct_emp = [r['wert'] for r in app_tables.games.search(game_id=cid, ta='Empowerment', reg=reg, runde=runde)]
+      pct_food = [r['wert'] for r in app_tables.games.search(game_id=cid, ta='Food', reg=reg, runde=runde)]
+      pct_ener = [r['wert'] for r in app_tables.games.search(game_id=cid, ta='Energy', reg=reg, runde=runde)]
+      tltl_pov = [r['tltl'] for r in app_tables.policies.search(ta='Poverty')]
+      gl_pov = [r['gl'] for r in app_tables.policies.search(ta='Poverty')]
+      tltl_emp = [r['tltl'] for r in app_tables.policies.search(ta='Empowerment')]
+      gl_emp = [r['gl'] for r in app_tables.policies.search(ta='Empowerment')]
+      tltl_ineq = [r['tltl'] for r in app_tables.policies.search(ta='Inequality')]
+      gl_ineq = [r['gl'] for r in app_tables.policies.search(ta='Inequality')]
+      tltl_food = [r['tltl'] for r in app_tables.policies.search(ta='Food')]
+      gl_food = [r['gl'] for r in app_tables.policies.search(ta='Food')]
+      tltl_ener = [r['tltl'] for r in app_tables.policies.search(ta='Energy')]
+      gl_ener = [r['gl'] for r in app_tables.policies.search(ta='Energy')]
+      lb = app_tables.budget.get(reg=reg, game_id=cid, yr=yr)
+      bud = lb['Bud_all_TA']
+      max_cost_pov = lb['Cost_poverty']
+      max_cost_food = lb['Cost_food']
+      max_cost_ener = lb['Cost_energy']
+      max_cost_ineq = lb['Cost_inequality']
+      max_cost_emp = lb['Cost_empowerment']
+      cost_pov = self.calc_cost(pct_pov, tltl_pov, gl_pov, max_cost_pov)
+      cost_emp = self.calc_cost(pct_emp, tltl_emp, gl_emp, max_cost_emp)
+      cost_ineq = self.calc_cost(pct_ineq, tltl_ineq, gl_ineq, max_cost_ineq)
+      cost_food = self.calc_cost(pct_food, tltl_food, gl_food, max_cost_food)
+      cost_ener = self.calc_cost(pct_ener, tltl_ener, gl_ener, max_cost_ener)    
+      total_cost = cost_pov + cost_emp + cost_ener + cost_food + cost_ineq
+      pct_of_budget = total_cost / bud * 100
+      if pct_of_budget > 100:
+        if pct_of_budget > 101:
+          pct_shown = str(int(pct_of_budget))
+        else:
+          pct_shown = round(pct_of_budget, 1)
+      self.budget_constraint.foreground = 'red'
+    else:
+      if pct_of_budget > 1:
+        pct_shown = str(int(pct_of_budget))
+      else:
+        pct_shown = round(pct_of_budget, 1)
+      self.budget_constraint.text = pct_shown
+      self.budget_constraint.foreground = 'green'
+    a=2
+
+
+
 
 
  
---
+++ client_code/HomeForm/pov_template/__init__.py
@@ -0,0 +1,15 @@
+from ._anvil_designer import pov_templateTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class pov_template(pov_templateTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    # Any code you write here will run before the form opens.
+    self.pol_name.text = self.item['pol_name']
+    self.pol_amount.text = self.item['pol_amt']
+

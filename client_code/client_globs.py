407411509e349584cd8a7f49f2d504f9feaa5391
grippe@blue-way.net <grippe@blue-way.net>
2025-04-09 09:46:46 +0000

    Edited client_globs

--- client_code/client_globs.py
+++ client_code/client_globs.py
@@ -2,18 +2,10 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-# This is a module.
-# You can define variables and functions here, and use them from any form. For example, in a top-level form:
-#
-#    from .. import Module1
-#
-#    Module1.say_hello()
-#
 ############
 ######      I am storing here client specific variables
 ###########
-
-the_game_i_am_in = ''
+my_game_id = ''
 my_reg = ''
 my_ministry = ''
 my_personal_game_id = ''

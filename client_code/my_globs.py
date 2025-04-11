3360aae90d0b7ba6e8aea68c4251e90e8120db72
grippe@blue-way.net <grippe@blue-way.net>
2025-04-11 17:38:16 +0000

    Created my_globs; Edited settings and ServerModule1; Deleted client_globs

---
+++ client_code/my_globs.py
@@ -0,0 +1,26 @@
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+#    from .. import Module1
+#    Module1.say_hello()
+###
+### these are CLIENT globals, ie all forms on the client side can access them
+my_game_id = ''
+my_reg = ''
+my_ministry = ''
+my_personal_game_id = ''
+my_step = 1
+game_step = 0
+###
+### comment this out in production and ask interactively
+not_played_by_players = ['us', 'af', 'cn', 'sa', 'la', 'pa', 'eu', 'se']
+###
+### below does not change, evah!
+roles = ['pov', 'ineq', 'emp', 'food', 'ener', 'fut']
+ro_nbr = [0, 1, 2, 3, 4, 5]
+regs = ['us', 'af', 'cn', 'me', 'sa', 'la', 'pa', 'ec', 'eu', 'se']
+re_nbr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -13,6 +13,35 @@ import matplotlib.pyplot as plt
 import anvil.mpl_util
 import string
 import numpy as np
+from . import my_globs
+
+@anvil.server.callable
+def set_roles(game_id):
+  roles = my_globs.roles
+  regs = my_globs.regs
+  ro_nbr = my_globs.ro_nbr
+  re_nbr = my_globs.re_nbr
+  npbp = my_globs.not_played_by_players
+  for re in regs:
+    if re in npbp:
+      for ro_n in ro_nbr:
+        app_tables.roles_taken.add_row(game_id=game_id, reg=re, role_nbr = ro_n, role=roles[ro_n], taken=2)  # 2 means role is filled by app
+    else:
+      for ro_n in ro_nbr:
+        app_tables.roles_taken.add_row(game_id=game_id, reg=re, role_nbr = ro_n, role=roles[ro_n], taken=0)
+  
+@anvil.server.callable
+def generate_id():
+  cid = ''.join(random.choices(string.ascii_uppercase, k=3))
+  a = random.randint(10, 99)
+  cid = cid + '-' + str(a) 
+  while app_tables.status.has_row(q.like(cid)):
+    cid = ''.join(random.choices(string.ascii_uppercase, k=4))
+    a = random.randint(10, 99)
+    d = random.randint(10, 99)
+    cid = cid + '-' + str(d) + '-' + str(a) 
+  return f"{cid}"
+  
 
 def timeitt(f):
     def timed(*args, **kw):
